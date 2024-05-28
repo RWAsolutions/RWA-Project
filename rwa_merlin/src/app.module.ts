df54b53e1e3ea1d609cfe8400a8a05fd7faa3ae1
AndrijaSkontra <askontra22@student.unizd.hr>
2024-05-28 11:19:03 +0200

    add connection service

--- rwa_merlin/src/app.module.ts
+++ rwa_merlin/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConnectionService } from './connection/connection.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConnectionService],
 })
 export class AppModule {}
---
+++ rwa_merlin/src/connection/connection.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import * as mysql from 'mysql';
+
+@Injectable()
+export class ConnectionService {
+  private connection: mysql.Connection;
+
+  constructor() {
+    this.connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : 'secret',
+      database : 'rwa_merlin'
+    });
+  }
+
+  public testConnection() {
+    this.connection.connect();
+ 
+    this.connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+      if (error) throw error;
+      console.log('The solution is: ', results[0].solution);
+    });
+
+    this.connection.end();
+  }
+}
