4c523d82ca193f457e5dea73b088a49aed81e36f
AndrijaSkontra <askontra22@student.unizd.hr>
2024-06-13 12:43:58 +0200

    add not found page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'login', component: FormComponent },
   { path: '', component: FormComponent },
-  { path: 'login2', component: LoginComponent }
+  { path: 'login2', component: LoginComponent },
+  { path: '**', component: NotFoundComponent }
 ];
