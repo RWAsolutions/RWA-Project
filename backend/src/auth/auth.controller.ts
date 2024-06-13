5621c15536d344fce8b05eae87ac41dee55190a9
AndrijaSkontra <askontra22@student.unizd.hr>
2024-06-13 11:02:45 +0200

    roles guard improvement

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -7,7 +7,7 @@ import { Public } from './decorators/public.decorator';
 @Controller('auth')
 export class AuthController {
 
-    constructor(private authService: AuthService){
+    constructor(private authService: AuthService) {
     }
 
 
