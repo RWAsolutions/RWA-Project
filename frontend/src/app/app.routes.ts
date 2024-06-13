767d2594774041a3986215a7e242081663244b85
AndrijaSkontra <askontra22@student.unizd.hr>
2024-06-13 09:17:36 +0200

    add login2 template driven form with 2 text fields

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    { path: 'login', component: FormComponent },
-    { path: '', component: FormComponent },
+  { path: 'login', component: FormComponent },
+  { path: '', component: FormComponent },
+  { path: 'login2', component: LoginComponent }
 ];
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  email = 'aklaric7@uni.prof.hr';
+  password = 'defaultpassword';
+}
