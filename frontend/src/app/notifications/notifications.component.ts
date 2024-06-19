918a4711e4a7368c5497b4a4be7d2c2100da7150
AndrijaSkontra <askontra22@student.unizd.hr>
2024-06-19 20:51:11 +0200

    add http get all notifications and implement a paginator

--- frontend/src/app/notifications/notifications.component.ts
+++ frontend/src/app/notifications/notifications.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+
+interface Notification {
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatPaginatorModule, MatDividerModule],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.scss'
 })
-export class NotificationsComponent {
+export class NotificationsComponent implements OnInit, AfterViewInit {
 
+  notifications: Notification[] = [];
+  displayedNotifications: Notification[] = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<Notification[]>('http://localhost:3000/notifications').subscribe(
+      notifications => {
+        this.notifications = notifications;
+        console.log('Notifications:', this.notifications);
+      });
+  }
+
+  ngAfterViewInit() {
+    this.paginator.page.subscribe(() => this.updateDisplayedNotifications());
+  }
+
+  updateDisplayedNotifications() {
+    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+    const endIndex = startIndex + this.paginator.pageSize;
+    this.displayedNotifications = this.notifications.slice(startIndex, endIndex);
+  }
 }
