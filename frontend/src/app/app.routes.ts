0e9bfbe3214371f242e327b5ef8f68be2b5df205
JosipKis <josip.kis2@gmail.com>
2024-06-19 21:40:28 +0200

    Zapocet rad na MyStudy komponenti

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { UserInfoComponent } from './user-info/user-info.component';
+import {MyStudyComponent} from "./my-study/my-study.component";
 
 export const routes: Routes = [
     { path: 'login', component: FormComponent },
     { path: '', component: FormComponent },
     { path: 'home', component: AppComponent},
     // { path: 'password-reset', component: PasswordResetComponent}
+  { path: 'my_study', component: MyStudyComponent},
   { path: 'user', component: UserInfoComponent },
   { path: '**', component: NotFoundComponent }, // this needs to be the last route
 ];
