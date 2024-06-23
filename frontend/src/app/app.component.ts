b8b5f4844f0d8ddbb9c589f985b4562976320db2
AndrijaSkontra <askontra22@student.unizd.hr>
2024-06-23 17:13:00 +0200

    implement banner on course card

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { Router, RouterOutlet } from '@angular/router';
 import { NotificationsComponent } from './notifications/notifications.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NotificationsComponent],
+  imports: [RouterOutlet, CommonModule, NotificationsComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,22 +17,7 @@ export class AppComponent {
   constructor(public router: Router) { }
 
   validNotificationRoute(): boolean {
-    let validUrls: string[] = ['/user'];
+    let validUrls: string[] = ['/user', '/courses', '/profil'];
     return validUrls.includes(this.router.url);
   }
 }
-=======
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-
-@Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    imports: [ RouterOutlet, HeaderComponent ],
-})
-export class AppComponent {
-  title = 'merlin-frontend';
-}
->>>>>>> khasnek
