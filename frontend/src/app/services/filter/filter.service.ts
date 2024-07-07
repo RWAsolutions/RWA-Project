74c232f2a513713086c8c71b8187ff693f50b4aa
Andrija Skontra <askontra22@student.unizd.hr>
2024-07-07 16:14:10 +0200

    optimize npm

--- frontend/src/app/services/filter/filter.service.ts
+++ frontend/src/app/services/filter/filter.service.ts
@@ -6,70 +6,70 @@ import { FilterDto } from "./filter.dto";
 import sortData from 'lodash';
 
 interface InfoDto {
-    courseID: number;
-    semester: {
-        semesterID: number;
-        semesterOrdinalNumber: number;
-    }
+  courseID: number;
+  semester: {
+    semesterID: number;
+    semesterOrdinalNumber: number;
+  }
 }
 
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class FilterService {
 
 
-   
-    constructor(private http: HttpClient){}
 
- 
+  constructor(private http: HttpClient) { }
 
-    getFilters(): Observable<FilterDto[]> {
-        return this.http.get<any[]>('http://localhost:3000/filter');
-        
+
+
+  getFilters(): Observable<FilterDto[]> {
+    return this.http.get<any[]>('http://localhost:3000/filter');
+
+  }
+
+  activateFilter(courses: CourseDto[], id: { studentID: number, profesorID: number }, selectedFilter: FilterDto): Observable<CourseDto[]> {
+    let type: 'student' | 'profesor' = 'student'
+    let userID: number = id.studentID
+
+    if (id.studentID === null) {
+      userID = id.profesorID
+      type = 'profesor'
     }
 
-    activateFilter(courses: CourseDto[], id: {studentID: number, profesorID: number}, selectedFilter: FilterDto): Observable<CourseDto[]> {
-        let type: 'student' | 'profesor' = 'student'
-        let userID: number = id.studentID
+    return this.http.get<InfoDto[]>(`http://localhost:3000/courses/${userID}/course-semester-info?type=${type}`).pipe(
+      map((info: InfoDto[]) => {
+        // console.log('Info received:', info);
+        return this.sortCoursesBySemester(courses, info, selectedFilter);
+      })
+    );
 
-        if(id.studentID === null) {
-            userID = id.profesorID
-            type = 'profesor'
-        }
-    
-        return this.http.get<InfoDto[]>(`http://localhost:3000/courses/${userID}/course-semester-info?type=${type}`).pipe(
-            map((info: InfoDto[]) => {
-                // console.log('Info received:', info);
-                return this.sortCoursesBySemester(courses, info, selectedFilter);
-            })
-        );
-        
-        
-    }
 
-    sortCoursesBySemester(courses: CourseDto[], info: InfoDto[], selectedFilter: FilterDto): CourseDto[] {
+  }
 
-        const infoMap = new Map<number,number>()
-        
-        info.forEach(item => {
-            infoMap.set(item.courseID,item.semester.semesterOrdinalNumber)
-        })
+  sortCoursesBySemester(courses: CourseDto[], info: InfoDto[], selectedFilter: FilterDto): CourseDto[] {
 
-        //* attaching the semester ordinal number for each course
-        courses.forEach(course => {
-            return course.semesterOrdinalNumber = infoMap.get(course.courseID) || 0
-        })
+    const infoMap = new Map<number, number>()
 
-        const sortedCourses = sortData.sortBy(courses,  course => {
-            const sortKey = course.semesterOrdinalNumber || 0
-            return selectedFilter.filterID === 1 ? sortKey : -sortKey
-        })
-        
-        console.log('These are the sorted courses:', sortedCourses);
-        
+    info.forEach(item => {
+      infoMap.set(item.courseID, item.semester.semesterOrdinalNumber)
+    })
 
-        return sortedCourses
-    }
+    //* attaching the semester ordinal number for each course
+    courses.forEach(course => {
+      return course.semesterOrdinalNumber = infoMap.get(course.courseID) || 0
+    })
+
+    const sortedCourses = sortData.sortBy(courses, course => {
+      const sortKey = course.semesterOrdinalNumber || 0
+      return selectedFilter.filterID === 1 ? sortKey : -sortKey
+    })
+
+    console.log('These are the sorted courses:', sortedCourses);
+
+
+    return sortedCourses
+  }
 }
